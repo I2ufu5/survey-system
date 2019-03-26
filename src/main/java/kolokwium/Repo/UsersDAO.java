b1b28270b9d2bc5b2e9916599ac767f21bff1e65
I2ufu5 <gamp3r@gmail.com>
2019-03-26 18:06:27 +0100

    CorsFiler added

--- src/main/java/kolokwium/Repo/UsersDAO.java
+++ src/main/java/kolokwium/Repo/UsersDAO.java
@@ -3,8 +3,10 @@ package kolokwium.Repo;
 import kolokwium.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Repository
+@CrossOrigin
 public interface UsersDAO extends JpaRepository<User,Long> {
     User findUserByAlbumNumber(Integer albumNumber);
 }
